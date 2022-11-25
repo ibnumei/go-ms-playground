878f2b84b144d04e359d10f454a1b5834df4d2b4
IBNU FAUZI MEIRIYANTO <19058263@MAC2UBJHCDITCD.local>
2022-11-26 00:47:14 +0700

    setup connection

--- internal/app/database/database.go
+++ internal/app/database/database.go
@@ -1,1 +1,24 @@
 package database
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func DatabaseConnection() *gorm.DB {
+	dbHost := os.Getenv("DB_HOST")
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	database := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=True&loc=Local", dbUsername, dbPassword, dbHost, port, database)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("can't connect to db")
+	}
+	return db
+}
