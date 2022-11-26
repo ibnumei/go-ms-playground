8627d643a32b7ef06981a6ff3fd0863a0343fe69
IBNU FAUZI MEIRIYANTO <19058263@MAC2UBJHCDITCD.local>
2022-11-26 21:54:28 +0700

    setup main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/subosito/gotenv"
+)
+
+func main() {
+	gotenv.Load()
+	route := gin.Default()
+
+	route.Run(":" + os.Getenv("PORT"))
+}
