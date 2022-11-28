8819b96aece18fba1c83b906453b0551b16c94ec
IBNU FAUZI MEIRIYANTO <19058263@MAC2UBJHCDITCD.local>
2022-11-29 00:11:36 +0700

    add loging to query raw db

--- internal/app/database/database.go
+++ internal/app/database/database.go
@@ -2,10 +2,13 @@ package database
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func DatabaseConnection() *gorm.DB {
@@ -15,8 +18,22 @@ func DatabaseConnection() *gorm.DB {
 	database := os.Getenv("DB_NAME")
 	port := os.Getenv("DB_PORT")
 
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: false,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  true,       // Disable color
+		},
+	)
+
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=True&loc=Local", dbUsername, dbPassword, dbHost, port, database)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// dsn := "root:password@tcp(127.0.0.1:3306)/go_playground?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		// Logger: logger.Default.LogMode(logger.Info),
+		Logger: newLogger,
+	})
 	if err != nil {
 		panic("can't connect to db")
 	}
